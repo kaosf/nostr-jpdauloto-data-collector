c332b6131013205e25a01b52edae2e6bdaa47c31
ka <ka.kaosf@gmail.com>
2023-08-26 00:50:09 +0900

    Impl

--- index.js
+++ index.js
@@ -1,37 +1,60 @@
-import { SimplePool } from "nostr-tools";
+import {
+  SimplePool,
+  nip19,
+  getEventHash,
+  getPublicKey,
+  getSignature,
+} from "nostr-tools";
+import "websocket-polyfill";
+import { readFileSync } from "fs";
+
+const relays = readFileSync("./relays.txt", "utf-8")
+  .split("\n")
+  .filter((x) => !x.match(/^#/))
+  .filter((x) => !(x === ""));
+const privkey = nip19.decode(readFileSync("./nsec.txt", "utf-8").trim()).data;
+const pubkey = getPublicKey(privkey);
+
+const kojiraPubkey =
+  "b3e43e8cc7e6dff23a33d9213a3e912d895b1c3e4250240e0c99dbefe3068b5f";
+
+const now = Math.floor(Date.now() / 1000);
 
 const pool = new SimplePool();
 
-let relays = ["wss://relay.example.com", "wss://relay.example2.com"];
+const sub = pool.sub(relays, [
+  {
+    kinds: [1],
+    authors: [kojiraPubkey],
+    since: now - 1 * 60 * 60,
+    "#t": "nostrquiz",
+  },
+]);
 
-let sub = pool.sub(
-  [...relays, "wss://relay.example3.com"],
-  [
-    {
-      authors: [
-        "32e1827635450ebb3c5a7d12c1f8e7b2b514439ac10a67eef3d9fd9c5c68e245",
-      ],
-    },
-  ],
-);
+const predictDau = () => {
+  return 271;
+};
 
 sub.on("event", (event) => {
-  // this will only be called once the first time the event is received
-  // ...
-});
+  console.log(event);
+  const replyId = event.id;
+  const content = `${predictDau()}`;
+  const ev = {
+    kind: 1,
+    created_at: Math.floor(Date.now() / 1000),
+    tags: [
+      ["e", replyId, "", "root"],
+      ["p", kojiraPubkey],
+    ],
+    content,
+    pubkey,
+  };
+  ev.id = getEventHash(ev);
+  ev.sig = getSignature(ev, privkey);
 
-let pubs = pool.publish(relays, newEvent);
-pubs.on("ok", () => {
-  // this may be called multiple times, once for every relay that accepts the event
-  // ...
+  try {
+    // pool.publish(relays, ev); // Comment out for safety now.
+  } catch {
+    // Do nothing
+  }
 });
-
-let events = await pool.list(relays, [{ kinds: [0, 1] }]);
-let event = await pool.get(relays, {
-  ids: ["44e1827635450ebb3c5a7d12c1f8e7b2b514439ac10a67eef3d9fd9c5c68e245"],
-});
-
-let relaysForEvent = pool.seenOn(
-  "44e1827635450ebb3c5a7d12c1f8e7b2b514439ac10a67eef3d9fd9c5c68e245",
-);
-// relaysForEvent will be an array of URLs from relays a given event was seen on
