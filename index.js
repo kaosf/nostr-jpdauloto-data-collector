27919a1e17e00bf14335971d657eb3528da6d68c
ka <ka.kaosf@gmail.com>
2023-09-18 20:26:45 +0900

    Tweak

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import { readFileSync, appendFileSync } from "fs";
 import { DateTime } from "luxon";
 import { execSync } from "child_process";
 
+console.log("Launched");
+
 const relays = readFileSync("./config/relays.txt", "utf-8")
   .split(/\n|\r\n|\r/)
   .filter((x) => !x.match(/^#/))
@@ -12,49 +14,45 @@ const relays = readFileSync("./config/relays.txt", "utf-8")
 const kojiraPubkey =
   "b3e43e8cc7e6dff23a33d9213a3e912d895b1c3e4250240e0c99dbefe3068b5f";
 
-const now = Math.floor(Date.now() / 1000);
-
 const pool = new SimplePool();
 
 const sub = pool.sub(relays, [
   {
     kinds: [1],
     authors: [kojiraPubkey],
-    since: now - 1 * 60 * 60,
+    since: Math.floor(Date.now() / 1000) - 1 * 60 * 60,
   },
 ]);
 
-const logs = readFileSync("./data/results.txt", "utf-8")
-  .split("\n")
-  .filter((x) => !(x === ""))
-  .map((x) => x.split(/\s+/));
-
 sub.on("event", (event) => {
   const result = event.content.match(/正解は(\d+)人/);
-  if (result !== null) {
-    const [_, num] = result;
-    console.log(event.created_at, num);
-    const createdAt = event.created_at;
-    const date = DateTime.fromSeconds(createdAt)
-      .setZone("Asia/Tokyo")
-      .minus({ days: 1 })
-      .toFormat("yyyy-MM-dd");
+  if (result === null) return;
+  const [_, num] = result;
+  console.log(event.created_at, num);
+  const createdAt = event.created_at;
+  const date = DateTime.fromSeconds(createdAt)
+    .setZone("Asia/Tokyo")
+    .minus({ days: 1 })
+    .toFormat("yyyy-MM-dd");
 
-    if (logs.find((x) => x[0] === event.id)) {
-      console.log(`event.id: ${event.id} already exists.`);
-      return;
-    }
+  const results = readFileSync("./data/results.txt", "utf-8")
+    .split(/\n|\r\n|\r/)
+    .filter((x) => !(x === ""))
+    .map((x) => x.split(/\s+/));
+  if (results.find((x) => x[0] === event.id)) {
+    console.log(`event.id: ${event.id} already exists.`);
+    return;
+  }
 
-    console.log(`event.id: ${event.id} is recorded.`);
-    appendFileSync(
-      "./data/results.txt",
-      `${event.id} ${event.created_at} ${date} ${num}\n`
-    );
-    try {
-      const stdout = execSync("bash data/update.sh");
-      console.log(stdout.toString());
-    } catch (e) {
-      console.log(e.toString());
-    }
+  console.log(`event.id: ${event.id} is recorded.`);
+  appendFileSync(
+    "./data/results.txt",
+    `${event.id} ${event.created_at} ${date} ${num}\n`
+  );
+  try {
+    const stdout = execSync("bash data/update.sh");
+    console.log(stdout.toString());
+  } catch (e) {
+    console.log(e.toString());
   }
 });
